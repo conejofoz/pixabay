f59255542ddecbeaea78033a1c1f941fae40a097
silvio-coelho <infinitygroupparaguay@gmail.com>
2022-01-19 15:48:33 -0300

    carregando imagens ok

---
+++ src/components/pixabay/Imagen.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <b-img :id="img.id" :src="img.previewURL" 
+            thumbnail fluid rounded
+            v-b-popover.hover.top="img.tags" title="Popover Title"
+        
+        >
+        </b-img>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Imagen",
+    props:["img"]
+}
+</script>
+
+<style lang="stylus" scoped>
+    
+</style>
---
+++ src/components/pixabay/Pixabay.vue
@@ -0,0 +1,50 @@
+<template>
+    <b-container fluid class="p-4 bg-dark text-white">
+        <b-row>
+            <b-col md="6">
+                <b-form-input @keypress.enter="buscarImagenes" v-model="buscar" placeholder="Ingrese la busca"></b-form-input>
+            </b-col>
+            <b-col md="1">
+                <b-button variant="danger" @click="buscarImagenes">Buscar</b-button>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col v-for="img in pixaImagenes" :key="img.id" md="2" class="py-2 text-center">
+                <!-- <b-img :id="img.id" :src="img.previewURL" thumbnail fluid></b-img> -->
+                <Imagen :img="img"></Imagen>
+            </b-col>    
+        </b-row>        
+    </b-container>
+</template>
+
+<script>
+import ServicioAPI from './ServicioAPI';
+import Imagen from './Imagen'
+
+export default {
+    name:"Pixabay",
+    components:{
+        Imagen,
+    },
+    data() {
+        return {
+            pixaImagenes:[],
+            buscar:''
+        }
+    },
+    methods:{
+        async buscarImagenes(){
+            const consulta = await ServicioAPI.getImagenes(this.buscar);
+            this.pixaImagenes = consulta.hits;
+            console.log(consulta);
+        }
+    },
+    mounted(){
+        this.buscarImagenes();
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    
+</style>
